831fca5f4e63433e3281bd9b860bb7406b4d7c6c
Esther <Esther@BlomkvistdeAir.ht.home>
2025-01-22 15:11:11 -0800

    Made a home page

---
+++ my-app/src/Pages/Cars.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cars = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>This is my car collection</h1>
+      <p>Welcome to the first sub-page!</p>
+    </div>
+  );
+};
+
+export default Cars;
---
+++ my-app/src/Pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Welcome to My Website</h1>
+      <p style={{ margin: '10px 0', fontSize: '18px', color: '#555' }}>
+        Explore the different sections of this website by clicking on the images below.
+      </p>
+
+
+
+      <div className="image-container"
+      style={{ display: 'flex', justifyContent: 'center', gap: '50px', flexWrap: 'wrap' }}>
+        <Link to="/Cars" className="image-link">
+          <img src="/images/pic1.jpg" alt="Page 1" />
+        </Link>
+        <Link to="/Games" className="image-link">
+          <img src="/images/pic2.jpg" alt="Page 2" />
+        </Link>
+        <Link to="/Projects" className="image-link">
+          <img src="/images/pic3.jpg" alt="Page 3" />
+        </Link>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
