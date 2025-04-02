720271fb828334d42b533d4585b806103192832a
snow2650 <snow113@students.cs.ubc.ca>
2025-04-02 13:38:21 -0700

    Added a picture

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Welcome to My Website</h1>
-      <p style={{ margin: '10px 0', fontSize: '18px', color: '#555' }}>
+      <p style={{ margin: '10px 0', fontSize: '19px', color: '#555' }}>
         Explore the different sections of this website by clicking on the images below.
       </p>
 
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="image-container"
       style={{ display: 'flex', justifyContent: 'center', gap: '50px', flexWrap: 'wrap' }}>
         <Link to="/Cars" className="image-link">
-          <img src="/images/pic1.jpg" alt="Page 1" />
+          <img src="public/images/40.jpg" alt="Page 1" />
         </Link>
         <Link to="/Games" className="image-link">
           <img src="/images/pic2.jpg" alt="Page 2" />
