b4c6eb538393647b7a9ba21c0b30a9bdfe90b45c
snow2650 <snow113@students.cs.ubc.ca>
2025-04-04 15:45:55 -0700

    Added a section and modified css

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -5,26 +5,33 @@ import './Home.css';
 const Home = () => {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Welcome to My Website</h1>
+      <h1>Welcome to My Playground</h1>
       <p style={{ margin: '10px 0', fontSize: '19px', color: '#555' }}>
         Explore the different sections of this website by clicking on the images below.
       </p>
 
-
+      <p style={{ margin: '10px 0', fontSize: '19px', color: '#555' }}>
+        ABOUT Me.
+      </p>
+      
       <div className="image-container"
       style={{ display: 'flex', justifyContent: 'center', gap: '50px', flexWrap: 'wrap' }}>
         <Link to="/Cars" className="image-link">
           <img src="/images/40.jpg" alt="Cars" />
         </Link>
         <Link to="/Games" className="image-link">
-          <img src="/images/40.jpg" alt="Games" />
+          <img src="/images/52.jpg" alt="Games" />
         </Link>
         <Link to="/Projects" className="image-link">
-          <img src="/images/40.jpg" alt="Projects" />
+          <img src="/images/31.jpg" alt="Projects" />
+        </Link>
+        <Link to="/Diary" className="image-link">
+          <img src="/images/46.jpg" alt="Diary" />
         </Link>
        
       </div>
     </div>
+    
   );
 };
 
