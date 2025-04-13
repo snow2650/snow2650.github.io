6c5840c98239660344d23f01196b4c09b2345bc9
snow2650 <snow113@students.cs.ubc.ca>
2025-04-12 17:13:06 -0700

    Modified import in About Me

--- my-app/src/Pages/AboutMe.js
+++ my-app/src/Pages/AboutMe.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Modal from 'react-modal';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,7 +8,6 @@ const AboutMe = () => {
   const navigate = useNavigate();
   const [selectedSection, setSelectedSection] = useState(null);
   //const [isModalOpen, setIsModalOpen] = useState(false);
-
   // debugging
   // useEffect(() => {
   //   console.log('AboutMe component mounted');
